b2ded04077fd99a7843a85688f9c66f6b982b01b
@picocreator (Eugene Cheah) <eugeneqin@gmail.com>
2023-08-11 02:12:33 +0000

    dragon test for v5 and count range

--- RWKV-v5/dragon_test.py
+++ RWKV-v5/dragon_test.py
@@ -10,7 +10,7 @@ import os
 # Check for argument, else throw error
 if len(sys.argv) < 2:
     print("No arguments supplied")
-    print("Usage: python3 dragon_test.py <model-path> [device]") # [tokenizer]")
+    print("Usage: python3 dragon_test.py <model-path> [device] [length]") # [tokenizer]")
     sys.exit(1)
 
 # download models: https://huggingface.co/BlinkDL
@@ -26,6 +26,11 @@ if DEVICE == "ref":
 
 if DEVICE is None:
     DEVICE = 'cuda'
+
+# Get the output length
+LENGTH=200
+if len(sys.argv) >= 4:
+    LENGTH=int(sys.argv[3])
 
 # Backward support for older format, we extract only cuda/cpu if its contained in the string
 if DEVICE.find('cuda') != -1:
@@ -53,10 +58,10 @@ model.completion("\n", max_tokens=1, temperature=1.0, top_p=0.7)
 prompt = "\nIn a shocking finding, scientist discovered a herd of dragons living in a remote, previously unexplored valley, in Tibet. Even more surprising to the researchers was the fact that the dragons spoke perfect Chinese."
 if IS_REF_RUN:
     print(f"--- DRAGON PROMPT (REF RUN) ---{prompt}", end='')
-    model.completion(prompt, stream_to_stdout=True, max_tokens=200, temperature=0.0)
+    model.completion(prompt, stream_to_stdout=True, max_tokens=LENGTH, temperature=0.0)
 else:
     print(f"--- DRAGON PROMPT ---{prompt}", end='')
-    model.completion(prompt, stream_to_stdout=True, max_tokens=200, temperature=1.0, top_p=0.7)
+    model.completion(prompt, stream_to_stdout=True, max_tokens=LENGTH, temperature=1.0, top_p=0.7)
 
 # Empty new line, to make the CLI formatting better
 print("")
