cf9cefadd8df97233fd0bb3b36b56e6d420602cd
@picocreator (Eugene Cheah) <eugeneqin@gmail.com>
2023-08-23 10:50:48 +0000

    fixing arg pos

--- notebook/github-runner/hf-upload.py
+++ notebook/github-runner/hf-upload.py
@@ -6,14 +6,14 @@ from huggingface_hub import HfApi
 api = HfApi()
 
 # Get the NOTEBOOK_FILE from the script first arg
-NOTEBOOK_FILE = sys.argv[2]
+NOTEBOOK_FILE = sys.argv[1]
 
 # Compute the notebook subdir from the notebook parent
 NOTEBOOK_SUBDIR = os.path.dirname(NOTEBOOK_FILE)
 
 # Get the repo ID from the second arg
 # if its not set/blank, defaults to rwkv-x-dev/rwkv-x-playground
-REPO_URI = sys.argv[1] if len(sys.argv) > 1 else ""
+REPO_URI = sys.argv[2] if len(sys.argv) > 1 else ""
 if REPO_URI == "":
     REPO_URI = "rwkv-x-dev/rwkv-x-playground"
 
