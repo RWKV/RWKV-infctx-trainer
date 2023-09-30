1255c6065324c81c673efe26b5ec3197e33caab8
picocreator (Eugene Cheah) <eugeneqin@gmail.com>
2023-09-30 11:39:10 +0800

    change init model to be safe by default

--- RWKV-v5/init_model.py
+++ RWKV-v5/init_model.py
@@ -5,7 +5,7 @@ from src.model import RWKV
 
 def init_model(
         layers, embedding_size, vocab_size, output_model_path, 
-        skip_if_exists=False, safe_init=False, emb_scale=0.0001
+        skip_if_exists=False, safe_init=True, emb_scale=0.0001
         # existing_model_path=None
         ):
     # Log the parameters
@@ -88,8 +88,7 @@ def init_model(
                 nn.init.orthogonal_(m[n], gain=gain * scale)
 
         # Ensure its mapped as a CPU & BF16
-        m[n] = m[n].cpu()
-        m[n] = m[n].bfloat16()
+        m[n] = m[n].cpu().bfloat16()
     
     # Save the model
     if safe_init:
