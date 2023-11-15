2f9a87a88bafc5795bba128be8f971362603deea
@picocreator (Eugene Cheah) <eugeneqin@gmail.com>
2023-11-15 01:30:32 +0000

    Forced bptt_truncated_learning to be true!

--- RWKV-v5/src/model.py
+++ RWKV-v5/src/model.py
@@ -279,6 +279,16 @@ class RWKV(L.LightningModule):
         self.substep_cuda_cache_clear = substep_cuda_cache_clear
         self.substep_logging = substep_logging
 
+        # Add warning that bptt_truncated_learning is forced to be true
+        # due to incomplete implementation of CUDA kernel for bptt_learning
+        #
+        # @TODO : remove this warning once the CUDA kernel, with state gradient, is implemented
+        if self.bptt_truncated_learning == False:
+            print("====================================================================")
+            print("[WARNING]: bptt_truncated_learning is set as true (was configured as false), due to incomplete implementation of CUDA kernel for bptt_learning")
+            print("====================================================================")
+            self.bptt_truncated_learning = True
+
         # Save the position loss params
         self.position_loss_bias = position_loss_bias
         self.position_loss_bias_in_validation = position_loss_bias_in_validation
