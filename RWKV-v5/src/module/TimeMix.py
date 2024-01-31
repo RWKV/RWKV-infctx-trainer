precision fixes
Signed-off-by: Daniel Goldstein <Dan@DESKTOP-UQ7GDK9>

RWKV-v5/src/module/TimeMix.py
@@ -319,8 +319,10 @@ class RWKV_TimeMix(JITModClass):
     def _forward_nocuda_optimized(self, x, last_state: tuple[torch.Tensor,torch.Tensor]):
         shift_state_out = x[:,-1]
 
-        # padding to support fast path for non-exact chunk size multiple sequence lengths
-        chunk_len = 32
+        # 24 is optimal chunk length (longer will use too much memory and cause precision problems or even numerical instability, shorter is inefficient)
+        chunk_len = 24
+
+        # padding to support fast path for non-exact chunk size multiple sequence lengths        
         n_padding = (chunk_len - x.size(-2) % chunk_len) % chunk_len
         if n_padding != 0:
             x = F.pad(x, [0, 0, 0, n_padding, 0, 0])

RWKV-v5/src/module/rwkv_inner.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 
 from torch import Tensor
 
-# 32 is optimal chunk length (longer will use too much memory, shorter is inefficient)
-def rwkv_inner(r,k,v,w,u,kv_state,chunk_len:int=32,precision_dtype:torch.dtype=torch.float32):
+# 24 is optimal chunk length (longer will use too much memory and cause precision problems or even numerical instability, shorter is inefficient)
+def rwkv_inner(r,k,v,w,u,kv_state,chunk_len:int=24,precision_dtype:torch.dtype=torch.float32):
     """
     expects
     r : (B,H,L,K)
@@ -33,6 +33,7 @@ def rwkv_inner(r,k,v,w,u,kv_state,chunk_len:int=32,precision_dtype:torch.dtype=t
         N = L // T
 
         # this has to be done to avoid numerical instability (inf/NaN) when w is used as a divisor up to chunk_length//2 places away (so precision_min_val^(T//2) has to be in fp range)
+        # NOTE - this does not account for the impact of the size of R, K so we currently use the chunk_len=32 numbers for chunk_len=24
         assert(precision_dtype == torch.float32 or precision_dtype == torch.float64)
         if precision_dtype == torch.float32:
             precision_min_val = 0.005 # good for fp32 (1.175e-38 ^ (1/16.0) < 0.00426)
@@ -80,28 +81,16 @@ def rwkv_inner(r,k,v,w,u,kv_state,chunk_len:int=32,precision_dtype:torch.dtype=t
         v = v.view(B,H,N,T,V)
         u = u.unsqueeze(2).to(r.dtype) # (1,H,1,1,K)
 
-        if precision_dtype == torch.float32:
-            r_length = torch.linalg.vector_norm(r, dim=-1).unsqueeze(-1) + 1e-14
-            k_length = torch.linalg.vector_norm(k, dim=-1).unsqueeze(-1) + 1e-14
-            r_unit_length = r / r_length
-            k_unit_length = k / k_length
-        else:
-            # just to appease torch.jit compiler - we don't use these values
-            r_length, k_length = r, k
-            r_unit_length, k_unit_length = r, k
-
         # parallel calculation of all intra-chunk attention contributions
         wc_log_offset = shifted_wc_log_cum[...,T//2:T//2+1,:] # B,H,N,1,K
         r_decay = (shifted_wc_log_cum - wc_log_offset).to(precision_dtype).exp() # B,H,N,T,K
         k_inv_decay = (wc_log_offset - wc_log_cum).to(precision_dtype).exp() # B,H,N,T,K
-        a = ((r_unit_length*r_decay) @ (k_unit_length*k_inv_decay).mT).to(r.dtype).tril(-1) # B,H,N,T,T
-        if precision_dtype == torch.float32:
-            a = a * (r_length * k_length)
+        a = ((r*r_decay) @ (k*k_inv_decay).mT).to(r.dtype).tril(-1) # B,H,N,T,T
         # add u term to attention (NOTE - the tril(-1) above zeroed the diagonal)
         a = a + torch.einsum('bhntk,bhntk->bhnt', r, u * k).diag_embed()
         out = a @ v # BHNTV
         # alternate way of adding in u
-        #outc = outc + torch.einsum('bhntk,bhntk,bhntv->bhntv', rc, u.unsqueeze(-2) * kc, vc) 
+        # out = out + torch.einsum('bhntk,bhntk,bhntv->bhntv', r, u * k, v) 
 
         # parallel precalculation of chunked (k*wk).mT@v for use in recurrent state calc below
         wkv = (k * w_inter).mT @ v # BHNKV