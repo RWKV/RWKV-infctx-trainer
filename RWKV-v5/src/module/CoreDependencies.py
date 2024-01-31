4deb31ad24c220fd049b02324713c6f4af8680af
Eugene Cheah (picocreator) <eugeneqin@gmail.com>
2024-01-31 16:05:04 +0000

    Automatically disable CUDA, if its not detected

--- RWKV-v5/src/module/CoreDependencies.py
+++ RWKV-v5/src/module/CoreDependencies.py
@@ -61,6 +61,10 @@ global RWKV_NO_CUDA
 if 'RWKV_NO_CUDA' not in globals():
     RWKV_NO_CUDA = os.getenv("RWKV_NO_CUDA", f"0").lower() in ("1", "true", "yes")
 
+# Enforce no cuda, if there is no cuda
+if torch.cuda is None or torch.cuda.is_available() == False or torch.cuda.device_count() <= 0:
+    RWKV_NO_CUDA = True
+
 # Disable torch compile if its not atleast v2.1.2
 if not IS_TORCH_2_1_2_COMPATIBLE:
     RWKV_TORCH_COMPILE = False
