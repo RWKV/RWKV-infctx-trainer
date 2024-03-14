7d443f59b8c93c8c6b7d82dfb41821261087b0cc
SmerkyG <SmerkyG@users.noreply.github.com>
2024-03-14 12:31:56 -0700

    v6 chanmix bugfix

--- RWKV-v5/src/module/ChannelMix.py
+++ RWKV-v5/src/module/ChannelMix.py
@@ -76,12 +76,14 @@ class RWKV_ChannelMix6_0(JITModClass):
     # - of output embedding of shape [batch_size, seq_len, embedding_size]
     # - and the last output state of shape [batch_size, state_size]
     @JITModMethod
-    def forward(self, x : torch.Tensor, xx : torch.Tensor):
+    def forward(self, x : torch.Tensor, last_state: torch.Tensor):
+        xx = torch.concat((last_state.unsqueeze(1), x[:, :-1]),
+                          dim=1)
         dxx = xx - x
         xk = x + dxx * self.time_maa_k
         xr = x + dxx * self.time_maa_r
         kv = self.value( torch.relu( self.key(xk) ) ** 2 )
-        return torch.sigmoid(self.receptance(xr)) * kv
+        return torch.sigmoid(self.receptance(xr)) * kv, x[:, -1]
 
 # Pure lambda implementation, of forwarding channel mix given the model weights
 # and the input tokens and states.
